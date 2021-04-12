a04dc58c57b918239502e6176453ec4c59a2ea8b
Ellie256 <61980311+Ellie256@users.noreply.github.com>
2021-04-12 12:12:06 -0400

    Added Merge Sort

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 void Swap(int a, int b, vector<int>& v); // send in indices that need to be swapped
 void ShellSort(vector<int> &v);
-void MergeSort(vector<int> &v);
+void Merge(vector<int> &v, int left, int mid, int right);
+void MergeSort(vector<int> &v, int left, int right);
 
 // implement two non-quadratic sorts and compare performance of both
 // Shell sort
@@ -15,8 +16,9 @@ void MergeSort(vector<int> &v);
 
 // changed
 int main() {
-    vector<int> v = {7, 4, 9, 3, 2, 8, 6, 5, 4, 7, 11, 2, 3, 9};
+    // vector<int> v = {7, 4, 9, 3, 2, 8, 6, 5, 4, 7, 11, 2, 3, 9};
     // vector<int> v = {12, 34, 54, 2, 3};
+    vector<int> v = {6, 5, 22, 10, 9, 1};
     cout << "[ ";
     for(int i = 0; i < v.size(); i++) {
         cout << v.at(i) << ", ";
@@ -35,8 +37,13 @@ int main() {
     cout << "Elapsed time to push in seconds: " << elapsed1.count() << endl;
     */
 
-    ShellSort(v);
-
+    // ShellSort(v);
+    MergeSort(v, 0, v.size() - 1);
+    cout << "[ ";
+    for(int i = 0; i < v.size(); i++) {
+        cout << v.at(i) << ", ";
+    }
+    cout << "]" << endl;
 
     return 0;
 }
@@ -47,6 +54,7 @@ void Swap(int a, int b, vector<int> &v) {
     v.at(b) = temp;
 }
 
+// function inspired by powerpoint, "Graphs-2", slide 34, by Amanpreet Kapoor
 void ShellSort(vector<int> &v) {
     int pass = 1;
     int k = (int) (log(v.size()) / log(2));
@@ -77,6 +85,62 @@ void ShellSort(vector<int> &v) {
     }
 }
 
-void MergeSort(vector<int> &v) {
+// function inspired by powerpoint, "Graphs-2", slide 49, by Amanpreet Kapoor
+void MergeSort(vector<int> &v, int left, int right) {
+    if (left < right) {
+        // m is the point where the array is divided into two subarrays
+        int mid = left + (right - left) / 2;
 
+        MergeSort(v, left, mid);
+        MergeSort(v, mid + 1, right);
+
+        // Merge the sorted subarrays
+        Merge(v, left, mid, right);
+    }
+}
+
+// function inspired by powerpoint, "Graphs-2", slide 50, by Amanpreet Kapoor
+// Merge two subarrays from arr
+void Merge(vector<int> &v, int left, int mid, int right) {
+    // Create X ← arr[left..mid] & Y ← arr[mid+1..right]
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int X[n1], Y[n2];
+
+    for (int i = 0; i < n1; i++) {
+        X[i] = v.at(left + i);
+    }
+    for (int j = 0; j < n2; j++) {
+        Y[j] = v.at(mid + 1 + j);
+    }
+
+    // Merge the arrays X and Y into arr
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2) {
+        if (X[i] <= Y[j]) {
+            v.at(k) = X[i];
+            i++;
+        }
+        else {
+            v.at(k) = Y[j];
+            j++;
+        }
+        k++;
+    }
+    // When we run out of elements in either X or Y append the remaining elements
+    while (i < n1) {
+        v.at(k) = X[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        v.at(k) = Y[j];
+        j++;
+        k++;
+    }
 }
