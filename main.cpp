dec843c50c1af0537b6fd1645401245846cc1800
Ellie256 <61980311+Ellie256@users.noreply.github.com>
2021-04-18 11:34:53 -0400

    Added Jump Search

--- main.cpp
+++ main.cpp
@@ -4,16 +4,23 @@
 #include <math.h>
 #include <fstream>
 #include <sstream>
+#include <math.h>
 #include "Song.h"
 using namespace std;
 
+// jump search and fibonacci search
+// Relearn Template Functions
+
 void Swap(int a, int b, vector<int>& v); // send in indices that need to be swapped
-void ShellSort(vector<int> &v);
-void Merge(vector<int> &v, int left, int mid, int right);
-void MergeSort(vector<int> &v, int left, int right);
-vector<Song> LoadData(string &path);
+void ShellSort(vector<int> &v, int option);
+void MergeSort(vector<int> &v, int left, int right, int option);
+void Merge(vector<int> &v, int left, int mid, int right, int option);
+int JumpSearch(vector<int> &v, int search, int option); // returns the index, returns -1 if not found
+int FibonacciSearch(vector<int> &v, int search, int option); // returns the index, returns -1 if not found
+void LoadData(string &path, vector<Song> &one, vector<Song> &two);
 
 int main() {
+    /*
     // vector<int> v = {7, 4, 9, 3, 2, 8, 6, 5, 4, 7, 11, 2, 3, 9};
     // vector<int> v = {12, 34, 54, 2, 3};
     vector<int> v = {6, 5, 22, 10, 9, 1};
@@ -23,7 +30,7 @@ int main() {
     }
     cout << "]" << endl;
 
-    /*
+
     // get current time
     std::chrono::high_resolution_clock::time_point start1 = std::chrono::high_resolution_clock::now();
 
@@ -33,15 +40,39 @@ int main() {
     // measure difference
     std::chrono::duration<double> elapsed1 = end1 - start1;
     cout << "Elapsed time to push in seconds: " << elapsed1.count() << endl;
-    */
+
 
     // ShellSort(v);
-    MergeSort(v, 0, v.size() - 1);
+    // MergeSort(v, 0, v.size() - 1);
     cout << "[ ";
     for(int i = 0; i < v.size(); i++) {
         cout << v.at(i) << ", ";
     }
     cout << "]" << endl;
+
+
+    vector<Song> shell;
+    vector<Song> merge;
+    string path = "example.csv";
+    LoadData(path, shell, merge);
+
+    for(int i = 0; i < shell.size(); i++) {
+        cout << "name: " << shell.at(i).GetName() << endl;
+        cout << "artist: " << shell.at(i).GetArtist() << endl;
+        cout << "popularity: " << shell.at(i).GetPopularity() << endl;
+        cout << "danceability: " << shell.at(i).GetDanceability() << endl;
+        cout << "duration: " << shell.at(i).GetDuration() << endl;
+        cout << "tempo: " << shell.at(i).GetTempo() << endl;
+        cout << "loudness: " << shell.at(i).GetLoudness() << endl;
+        cout << "year: " << shell.at(i).GetYear() << endl;
+        cout << endl;
+    }
+    */
+
+    //                  0, 1, 2, 3, 4, 5, 6, 7,  8,  9,  10, 11, 12,  13,  14,  15,  16
+    vector<int> arr = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 700 };
+    int search = 700;
+    cout << "Index: " << JumpSearch(arr, search, 0);
 
     return 0;
 }
@@ -53,16 +84,21 @@ void Swap(int a, int b, vector<int> &v) {
 }
 
 // function inspired by powerpoint, "Graphs-2", slide 34, by Amanpreet Kapoor
-// The gap function is based on Hibbard's sequence
-void ShellSort(vector<int> &v) {
-    int pass = 1;
+void ShellSort(vector<int> &v, int option) {
+    // Initialize k to be the largest so that gap is not larger than the size of the vector
     int k = (int) (log(v.size()) / log(2));
+    // The gap function is based on Hibbard's sequence
     int gap = pow(2, k) - 1;
+
+    // The gap will decrease until it is 1
     while(gap > 0) {
+
+        // Compare elements that are a gap away from each otehr
         for(int i = 0; i < v.size() - gap; i++) {
             if(v.at(i) > v.at(i + gap)) {
                 Swap(i, i+gap, v);
 
+                // If two elements are swapped, then check previous elements that are gaps away
                 for(int j = i; j >= gap; j -= gap) {
                     if(v.at(j) < v.at(j - gap)) {
                         Swap(j, j-gap, v);
@@ -73,34 +109,30 @@ void ShellSort(vector<int> &v) {
                 }
             }
         }
-        cout << "Gap " << gap << ": [ ";
-        for(int i = 0; i < v.size(); i++) {
-            cout << v.at(i) << ", ";
-        }
-        cout << "]" << endl;
-        pass++;
+
+        // Update the gap
         k--;
         gap = pow(2, k) - 1;
     }
 }
 
 // function inspired by powerpoint, "Graphs-2", slide 49, by Amanpreet Kapoor
-void MergeSort(vector<int> &v, int left, int right) {
+void MergeSort(vector<int> &v, int left, int right, int option) {
     if (left < right) {
         // m is the point where the array is divided into two subarrays
         int mid = left + (right - left) / 2;
 
-        MergeSort(v, left, mid);
-        MergeSort(v, mid + 1, right);
+        MergeSort(v, left, mid, option);
+        MergeSort(v, mid + 1, right, option);
 
         // Merge the sorted subarrays
-        Merge(v, left, mid, right);
+        Merge(v, left, mid, right, option);
     }
 }
 
 // function inspired by powerpoint, "Graphs-2", slide 50, by Amanpreet Kapoor
 // Merge two subarrays from arr
-void Merge(vector<int> &v, int left, int mid, int right) {
+void Merge(vector<int> &v, int left, int mid, int right, int option) {
     // Create X ← arr[left..mid] & Y ← arr[mid+1..right]
     int n1 = mid - left + 1;
     int n2 = right - mid;
@@ -144,14 +176,11 @@ void Merge(vector<int> &v, int left, int mid, int right) {
     }
 }
 
-vector<Song> LoadData(string &path) {
+void LoadData(string &path, vector<Song> &one, vector<Song> &two) {
     // Put data from file into ifstream
     ifstream inFile;
     // Open the file
     inFile.open(path);
-
-    // Make a vector of Songs
-    vector<Song> songs;
 
     string lineFromFile;
     // Ignore headers
@@ -170,6 +199,8 @@ vector<Song> LoadData(string &path) {
         // Get artist
         string artist;
         getline(lineStream, artist, ',');
+        artist.erase(0, 2);
+        artist.erase(artist.end() - 2, artist.end());
 
         // Get popularity
         string s;
@@ -200,8 +231,53 @@ vector<Song> LoadData(string &path) {
         Song newSong(name, artist, popularity, danceability, duration, tempo, loudness, year);
 
         // Put new Song into vector
-        songs.push_back(newSong);
+        one.push_back(newSong);
+        two.push_back(newSong);
+    }
+}
+
+// function inspired by GeeksforGeeks article, "Jump Search"
+int JumpSearch(vector<int> &v, int search, int option) {
+    int jump = sqrt(v.size());
+    int minIndex = 0;
+    int maxIndex;
+
+    // Jump
+    for(int i = 0; i < v.size(); i += jump) {
+        if(search == v.at(i)) {
+            return i;
+        }
+        else if(search < v.at(i)) {
+            maxIndex = i;
+            break;
+        }
+        else {
+            minIndex = i;
+        }
     }
 
-    return songs;
+    // Check for case that search is near the end of v
+    if(search > v.at(maxIndex)) {
+        minIndex = maxIndex;
+        maxIndex = v.size();
+    }
+
+    // Linear
+    for(int i = minIndex + 1; i < maxIndex; i++) {
+        if(search == v.at(i)) {
+            return i;
+        }
+    }
+
+    // If nothing has been returned, it doesn't exist
+    return -1;
 }
+
+int FibonacciSearch(vector<int> &v, int search, int option) {
+
+}
+
+
+
+
+
